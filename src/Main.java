b9bedf795be0176a1ce325e08a0ac94662ea49a2
clark7jj <clark7jj@mail.uc.edu>
2026-09-18 14:35:13 -0400

    Completed Task 04.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+int numToExamine = 4;
+System.out.println("A value of 0 means your number is even(no remainder when dividing by 2)");
+System.out.println("A value of 1 means your number is odd(no remainder when dividing by 2)");
+int result = numToExamine % 2;
+System.out.println("The result of the modulo operation is: " + result);
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
